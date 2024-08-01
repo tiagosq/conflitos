5b549a81ed82a584365c03b7c5bda1f3de37f370
Tiago Quadros <contato@tiagoquadros.com.br>
2024-08-01 19:41:52 -0300

    Contador Hooks

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import { useState } from 'react';
+
+const App = () => {
+  const [count, setCount] = useState(0);
+
+  const incrementar = () => {
+    setCount(count + 1);
+  };
+
+  const decrementar = () => {
+    setCount(count - 1);
+  };
+
+  return (
+    <div>
+      <h1>Contador: {count}</h1>
+      <button onClick={incrementar}>Incrementar</button>
+      <button onClick={decrementar}>Decrementar</button>
+    </div>
+  );
+};
+
+export default App;
